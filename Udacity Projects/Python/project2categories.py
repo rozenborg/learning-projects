713226b49224757b7735aa744307f5246b5eaefa
Michael Rosenberg <writerosenberg@gmail.com>
2016-05-21 19:43:53 +0100

    finishing touches to quiz, and swapped content of original and alternate files.

--- Udacity Projects/Python/project2categories.py
+++ Udacity Projects/Python/project2categories.py
@@ -1,10 +1,8 @@
-# all quiz text, lists, and dictionaries are listed at the top
-
 '''quiz texts'''
 
-quizCategoryBank = ["easy", "medium", "hard"]
+quizCategoryBank = ["innovation", "existentialism", "power"]
 
-quizEasyText = """Here's to the __1__ ones. The misfits. The rebels. The troublemakers.
+quizInnovationText = """Here's to the __1__ ones. The misfits. The rebels. The troublemakers.
 The round pegs in the square holes. The ones who see things __2__.
 They're not fond of rules. And they have no respect for the __3__.
 You can quote them, disagree with them, glorify or vilify them.
@@ -19,7 +17,7 @@ explores as many of them as __4__. Religions that teach pity,
 self-contempt, humility, self-restraint and guilt are incorrect.
 The __5__ life is ever changing, challenging, devoid of regret, intense, creative and risky."""
 
-quizHardText = """The ideas of the ruling __1__ are in every epoch the ruling ideas,
+quizPowerText = """The ideas of the ruling __1__ are in every epoch the ruling ideas,
 i.e. the class which is the ruling material force of __2__,
 is at the same time its ruling intellectual force.
 The class which has the means of material production at its disposal,
@@ -29,11 +27,11 @@ The ruling ideas are nothing more than the ideal expression of the dominant mate
 the __5__ material relationships grasped as ideas."""
 
 
-'''answer banks'''
+'''Answer Banks'''
 
-answersEasy = ["crazy", "differently", "status quo", "ignore", "genius", "crazy"]
-answersMedium = ["onesself", "dangerously", "infinite", "possible", "good"]
-answersHard = ["class", "society", "production", "subject", "dominant"]
+answersInnovation = ["crazy", "differently", "status quo", "ignore", "genius", "crazy"]
+answersExistentialism = ["onesself", "dangerously", "infinite", "possible", "good"]
+answersPower = ["class", "society", "production", "subject", "dominant"]
 
 
 '''display messages'''
@@ -53,81 +51,63 @@ loseMessage = "\n" + "\n" + "Ehhh, sorry, you are out of tries. We still think y
 '''dictionaries'''
 
 quizTexts = {
-	"easy": quizEasyText,
-	"medium": quizExistentialText,
-	"hard": quizPowerText,
+	"innovation": quizInnovationText,
+	"existentialism": quizExistentialText,
+	"power": quizPowerText,
 }
 
 quizAnswers = {
-	"easy": answersEasy,
-	"medium": answersMedium,
-	"hard": answersPower,
+	"innovation": answersInnovation,
+	"existentialism": answersExistentialism,
+	"power": answersPower,
 }
 
 
 '''code'''
 
-# this function finds and displays the next blank in a text
-
 def nextBlank(blankIndex, selectedQuizText):
 	blankIndex += 1
+	# print "blankIndex: ", blankIndex
+	# print "selectedQuizText: ", selectedQuizText
 	blank = "__" + str(blankIndex) + "__"
+	# print "blank: ", blank
 	if blank in selectedQuizText:
 		return blank
 
-# this function runs the actual quiz game
-
 def quiz(): 
-
-# this section displays the intro message and prompts the user to select a difficulty
 
 	introMessage = welcome
 	while True:
 		quizCategory = raw_input("\n" + introMessage + "\n" + " - " + "\n - ".join(quizCategoryBank) + "\n" + "\n" + "Your selection: ")
 		if quizCategory.lower() in quizCategoryBank:
 			break
-		introMessage = welcomeFail
-
-# the selected quiz is drawn from the content above, and the user is notified of their choice and number of tries for each guess
+		introMessage = welcomeFail #, ", ".join(quizCategoryBank)
 
 	selectedQuizText = quizTexts[quizCategory]
 	selectedAnswerBank = quizAnswers[quizCategory]
 
 	print "\n" + "You have chosen " + quizCategory + "." + "\n" + "\n" + "You will have three tries for each blank. Ride or die byatch!"
 
-# dear reviewer person: can you please give me guidance on how to refactor the code below?
 
+# dear reviewer person: can you please give me guidance on how to refactor the code below?
+	
+
+	# while triesLeft > 0:
 	blankIndex = 0
 	triesLeft = 3
-
-# this loop runs as long as their are blanks left in the quiz text (when there are no blanks left, the user will have won)
-
 	while nextBlank(blankIndex, selectedQuizText) != None:
-
-		# if the user runs out of tries they lose
-
 		if triesLeft == 0:
 			return loseMessage
-
-		# while the game is still live the latest quiz text is displayed and a prompt for the next guess is displayed
-
 		print "\n" + "\n" + selectedQuizText + "\n" + "\n"
 		response = raw_input("What word or phrase belongs in " + str(nextBlank(blankIndex, selectedQuizText)) + ":  ")
-		
-		# if the user guesses incorrectly, they are notified and the number of tries is reduced and displayed
-
 		if response.lower() != selectedAnswerBank[blankIndex]:
 			triesLeft -= 1
 			print answerIncorrect + str(triesLeft)
-
-		# if the answer is correct the word is filled in on the quiz, the quiz is displayed in its updated format, and the user is asked to enter their guess for the next blank
-
 		else:
 			print answerCorrect
 			selectedQuizText = selectedQuizText.replace(nextBlank(blankIndex, selectedQuizText), response)
 			blankIndex += 1
 			triesLeft = 3
-
 	return winMessage
 
 print quiz()
